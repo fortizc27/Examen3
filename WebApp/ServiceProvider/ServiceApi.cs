bd61fbedb85599b333815fd0908da64a79952d20
fortiz <fortiz@gbm.net>
2021-12-17 21:21:42 -0600

    AppProducto

--- WebApp/ServiceProvider/ServiceApi.cs
+++ WebApp/ServiceProvider/ServiceApi.cs
@@ -1,11 +1,39 @@
-﻿using System;
+﻿using Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace WebApp
 {
     public class ServiceApi
     {
+        private readonly HttpClient client;
+
+        public ServiceApi(HttpClient client)
+        {
+            this.client = client;
+        }
+
+
+        public async Task<IEnumerable<ProductoEntity>> ProductoGet()
+        {
+
+            var result = await client.ServicioGetAsync<IEnumerable<ProductoEntity>>("api/Producto");
+            return result;
+
+        }
+
+
+        public async Task<ProductoEntity> ProductoGetById(int id)
+        {
+            var result = await client.ServicioGetAsync<ProductoEntity>("api/Producto/" + id);
+
+            if (result.CodeError is not 0) throw new Exception(result.MsgError);
+
+            return result;
+
+        }
     }
 }
