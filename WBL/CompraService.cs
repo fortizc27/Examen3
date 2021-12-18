0fc372ef4154eb36bc96e6d70e6c6a7dff4b6693
fsoza1976 <fsoza1976@gmail.com>
2021-12-17 19:04:15 -0600

    WBL Cliente.

---
+++ WBL/CompraService.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BD;
+using Entity;
+
+namespace WBL
+{
+    public interface ICompraService
+    {
+        Task<DBEntity> Create(CompraEntity entity);
+        Task<DBEntity> Delete(CompraEntity entity);
+        Task<IEnumerable<CompraEntity>> Get();
+        Task<IEnumerable<CompraEntity>> GetLista();
+        Task<CompraEntity> GetById(CompraEntity entity);
+        Task<DBEntity> Update(CompraEntity entity);
+    }
+
+    public class CompraService : ICompraService
+    {
+        private readonly IDataAccess sql;
+
+        public CompraService(IDataAccess _sql)
+        {
+            sql = _sql;
+        }
+
+
+        #region MetodosCRUD
+
+        //Método GET
+        public async Task<IEnumerable<CompraEntity>> Get()
+        {
+            try
+            {
+                var result = sql.QueryAsync<CompraEntity>("dbo.CompraObtener");
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        //Método GET Lista
+        public async Task<IEnumerable<CompraEntity>> GetLista()
+        {
+            try
+            {
+                var result = sql.QueryAsync<CompraEntity>("dbo.CompraLista");
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        //Método GET ID
+        public async Task<CompraEntity> GetById(CompraEntity entity)
+        {
+            try
+            {
+                var result = sql.QueryFirstAsync<CompraEntity>("dbo.CompraObtener", new { entity.IdCompra });
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+
+        //Método CREATE
+        public async Task<DBEntity> Create(CompraEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("dbo.CompraInsertar", new
+                {
+                    entity.ClientesId,
+                    entity.ProductoId,
+                    entity.FechaCompra,
+                    entity.Monto,
+                    entity.Impuesto,
+                    entity.Total,
+                    entity.Observaciones,
+                    entity.Estado
+                });
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+
+        //Método UPDATE
+        public async Task<DBEntity> Update(CompraEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("dbo.CompraActualizar", new
+                {
+                    entity.IdCompra,
+                    entity.ClientesId,
+                    entity.ProductoId,
+                    entity.FechaCompra,
+                    entity.Monto,
+                    entity.Impuesto,
+                    entity.Total,
+                    entity.Observaciones,
+                    entity.Estado
+                });
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+
+        //Método ELIMINAR
+        public async Task<DBEntity> Delete(CompraEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("dbo.CompraEliminar", new
+                {
+                    entity.IdCompra
+                });
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+
+        #endregion
+
+    }
+}
+
+
