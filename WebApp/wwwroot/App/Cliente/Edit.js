c08726e5a6557dc8c2b7f6d2446f9003e79dcae6
Nohelia Sobalvarro <arial8520@gmail.com>
2021-12-18 01:18:02 -0600

    Agregue servicios controladores pagina etc

--- WebApp/wwwroot/App/Cliente/Edit.js
+++ WebApp/wwwroot/App/Cliente/Edit.js
@@ -1,2 +1,35 @@
 "use strict";
+var ClienteEdit;
+(function (ClienteEdit) {
+    var Entity = $("#AppEdit").data('entity');
+    var Formulario = new Vue({
+        data: {
+            Formulario: "#FormEdit",
+            Entity: Entity
+        },
+        methods: {
+            Save: function () {
+                if (BValidateData(this.Formulario)) {
+                    Loading.fire("Guardando");
+                    App.AxiosProvider.ClienteGuardar(this.Entity).then(function (data) {
+                        Loading.close();
+                        if (data.CodeError == 0) {
+                            Toast.fire({ title: "Registro almacenado.", icon: "success" }).then(function () { return window.location.href = "Cliente/Grid"; });
+                        }
+                        else {
+                            Toast.fire({ title: data.MsgError, icon: "error" });
+                        }
+                    });
+                }
+                else {
+                    Toast.fire({ title: "Campos obligatorios." });
+                }
+            }
+        },
+        mounted: function () {
+            CreateValidator(this.Formulario);
+        }
+    });
+    Formulario.$mount("#AppEdit");
+})(ClienteEdit || (ClienteEdit = {}));
 //# sourceMappingURL=Edit.js.map
--- WebApp/wwwroot/App/Cliente/Edit.ts
+++ WebApp/wwwroot/App/Cliente/Edit.ts
@@ -1,1 +1,40 @@
-﻿
+﻿namespace ClienteEdit {
+
+    var Entity = $("#AppEdit").data('entity');
+
+    var Formulario = new Vue(
+        {
+            data:
+            {
+                Formulario: "#FormEdit",
+                Entity: Entity
+            },
+            methods:
+            {
+                Save() {
+                    if (BValidateData(this.Formulario)) {
+                        Loading.fire("Guardando");
+                        App.AxiosProvider.ClienteGuardar(this.Entity).then(data => {
+                            Loading.close();
+
+                            if (data.CodeError == 0) {
+                                Toast.fire({ title: "Registro almacenado.", icon: "success" }).then(() => window.location.href = "Cliente/Grid")
+                            }
+                            else {
+                                Toast.fire({ title: data.MsgError, icon: "error" })
+                            }
+                        })
+                    }
+                    else {
+                        Toast.fire({ title: "Campos obligatorios." });
+                    }
+                }
+            },
+            mounted() {
+                CreateValidator(this.Formulario)
+            }
+        }
+    );
+
+    Formulario.$mount("#AppEdit")
+}
